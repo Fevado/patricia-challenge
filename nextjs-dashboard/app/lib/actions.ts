5a5a0e51afec41b2375f51e0ee39cbe7a4952ea6
favoured mwange <mwangefavoured@gmail.com>
2026-09-13 19:31:50 +0300

    adds ctry atch error

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -28,10 +28,17 @@ export async function createInvoice(formData:FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
-    await sql`
+    try{
+      await sql`
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
+    } catch(error){
+        console.error(error);
+        return {
+            message: 'Database Error: Failed to Create Invoice.',
+        };
+    }
 
     revalidatePath('/dashboard/invoices')
     redirect('/dashboard/invoices');
@@ -49,11 +56,17 @@ export async function updateInvoice(id: string, formData: FormData){
 
     const amountInCents = amount * 100;
 
-    await sql`
+    try{
+      await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents} status = ${status}
     WHERE id = ${id}
     `;
+    } catch (error) {
+        console.error(error);
+        return {message: 'Database Error: Failed to Update Invoice.',
+        };
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
@@ -63,3 +76,4 @@ export async function deleteInvoice(id:string) {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/ddashboard/invoices');
 };
+
